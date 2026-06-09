c0201b8e8158da348b84dd1f18d865611b773c27
kyoleht <rafaelferreiradasilva1812@gmail.com>
2026-06-09 01:29:42 -0300

    Adicionando nova .properties para tratativa de features e testes

--- src/main/java/com/dev/rafa/springapp/SpringAppApplication.java
+++ src/main/java/com/dev/rafa/springapp/SpringAppApplication.java
@@ -2,8 +2,11 @@ package com.dev.rafa.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
+
 public class SpringAppApplication {
 
     public static void main(String[] args) {
